266a4a9029a60c807bdc45795afb2ec518253d61
Marco Mercado <mmerc007@ucr.edu>
2018-03-24 00:37:34 -0700

    first tutorial completed neural network with tww hidden layers started to train right

--- Artificial_Neural_Networks/net.py
+++ Artificial_Neural_Networks/net.py
@@ -17,13 +17,14 @@ labelencoder_X_1 = LabelEncoder()                          # we encode the count
 labelencoder_X_2 = LabelEncoder()                                         # we encode the country into a 0 1 2 to make it easier for the net one object to encode
 X[:, 1] = labelencoder_X_1.fit_transform(X[:, 1]) # the first endtry is all rows and 1 index or second colomn after this line it's encoded
 X[:, 2]= labelencoder_X_2.fit_transform(X[:, 2]) # the first endtry is all rows and 2 index or second colomn after this line it's encoded
-onehotencoder = OneHotEncoder(categorical_features = [0])
-X = onehotencoder.fit_transform(X).toarray()
+onehotencoder = OneHotEncoder(categorical_features = [1])
+X = onehotencoder.fit_transform(X).toarray() # devide the contry colomn into 3 to make it independent 
+X = X[:,1:] #then we get rid of one of the new colomns created
 
 
 # Splitting the dataset into the Training set and Test set
 from sklearn.cross_validation import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 0)
+X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size = 0.2, random_state = 0)
 
 # Feature Scaling
 from sklearn.preprocessing import StandardScaler
@@ -31,8 +32,41 @@ sc = StandardScaler()
 X_train = sc.fit_transform(X_train)
 X_test = sc.transform(X_test)
 
+# Making the neural network
+
+#importing libraries 
+import keras 
+from keras.models import Sequential 
+from keras.layers import Dense
+
+
+
+#initilizing the neural net
+classifier = Sequential()   #geting an object to initizeing the net
+ 
+
+#first hidden layer
+# adding th layers sicne 11 independent variables then we get 11 first notes in the first layers.
+# we have to choose the activatio neural network: we choose the retifier for the hidden and the sigmoid for the output since is binary
+classifier.add(Dense(  activation='relu',input_dim=11, units= 6, kernel_initializer='uniform' )) # adding layers in hidden layer the average in the input and the output otherwise 11+1/2 we have to do cross validation
+#second hidden layer #get rid of units or the first entries becuase it will read from the previosu
+#uints is the output layer
+classifier.add(Dense(  activation='relu', units= 6, kernel_initializer='uniform' )) # adding layers in hidden layer the average in the input and the output otherwise 11+1/2 we have to do cross validation
+#final layer 
+classifier.add(Dense(  activation='sigmoid', units= 1, kernel_initializer='uniform' ))
+#compile the network 
+# adam is stochastic gradient decent, then loss function
+# loss function vary if it's none binary
+classifier.compile(optimizer='adam',loss='binary_crossentropy', metrics=['accuracy']) # adam is stochastic gradient decent, then loss function
+
+
+
+
 # Fitting classifier to the Training set
-# Create your classifier here
+# training by bash 
+classifier.fit(X_train, y_train, batch_size=10, epochs=100)
+
+
 
 # Predicting the Test set results
 y_pred = classifier.predict(X_test)
