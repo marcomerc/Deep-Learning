6733d9e8e126d43251f87ad3f3b7ff877ff3c990
Marco Mercado <mmerc007@ucr.edu>
2018-03-24 22:39:34 -0700

    choosing hyperparameters

--- Artificial_Neural_Networks/net.py
+++ Artificial_Neural_Networks/net.py
@@ -20,66 +20,45 @@ X[:, 2]= labelencoder_X_2.fit_transform(X[:, 2]) # the first endtry is all rows 
 onehotencoder = OneHotEncoder(categorical_features = [1])
 X = onehotencoder.fit_transform(X).toarray() # devide the contry colomn into 3 to make it independent 
 X = X[:,1:] #then we get rid of one of the new colomns created
-
-
 # Splitting the dataset into the Training set and Test set
 from sklearn.cross_validation import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size = 0.2, random_state = 0)
-
 # Feature Scaling
 from sklearn.preprocessing import StandardScaler
 sc = StandardScaler()
 X_train = sc.fit_transform(X_train)
 X_test = sc.transform(X_test)
-
 # Making the neural network
-
 #importing libraries 
 import keras 
 from keras.models import Sequential 
 from keras.layers import Dense
-
-
-
 #initilizing the neural net
 classifier = Sequential()   #geting an object to initizeing the net
- 
-
 #first hidden layer
 # adding th layers sicne 11 independent variables then we get 11 first notes in the first layers.
 # we have to choose the activatio neural network: we choose the retifier for the hidden and the sigmoid for the output since is binary
 classifier.add(Dense(  activation='relu',input_dim=11, units= 6, kernel_initializer='uniform' )) # adding layers in hidden layer the average in the input and the output otherwise 11+1/2 we have to do cross validation
+classifier.add(Dropout(p = 0.1)) #adding the dropout regulatro it means disable 10 percent of the neural network
 #second hidden layer #get rid of units or the first entries becuase it will read from the previosu
 #uints is the output layer
 classifier.add(Dense(  activation='relu', units= 6, kernel_initializer='uniform' )) # adding layers in hidden layer the average in the input and the output otherwise 11+1/2 we have to do cross validation
+classifier.add(Dropout(p = 0.1)) #adding the dropout regulatro it means disable 10 percent of the neural network
 #final layer 
 classifier.add(Dense(  activation='sigmoid', units= 1, kernel_initializer='uniform' ))
 #compile the network 
 # adam is stochastic gradient decent, then loss function
 # loss function vary if it's none binary
 classifier.compile(optimizer='adam',loss='binary_crossentropy', metrics=['accuracy']) # adam is stochastic gradient decent, then loss function
-
-
-
-
 # Fitting classifier to the Training set
 # training by bash 
 classifier.fit(X_train, y_train, batch_size=10, epochs=100)
-
-
-
-
-
-
 # Predicting the Test set results
 y_pred = classifier.predict(X_test)
 y_pred = (y_pred > 0.5) #change everything greater to 0.5 becomes 1 and else 0
 # Making the Confusion Matrix
 from sklearn.metrics import confusion_matrix
 cm = confusion_matrix(y_test, y_pred) 
-
-
-
 #k validation 
 from keras.wrappers.scikit_learn import KerasClassifier
 from sklearn.model_selection import cross_val_score
@@ -103,12 +82,35 @@ accuracies = cross_val_score(estimator =classifier,X =X_train,y = y_train, cv = 
 mean = accuracies.mean() #computing the mean 
 variance = accuracies.std()
 
+# we are going to use drop out regulitor or half it half of the layer edit at the top
 
-
-
-
-
-
+from keras.wrappers.scikit_learn import KerasClassifier
+from sklearn.model_selection import cross_val_score
+# A function builds arequitecture of neural net 
+import keras 
+from keras.models import Sequential 
+from keras.layers import Dense
+from sklearn.model_selection import GridSearchCV  ## selecting parameters 
+def build_classifier(optimizer):
+    import keras 
+    from keras.models import Sequential 
+    from keras.layers import Dense
+    classifier = Sequential()   #geting an object to initizeing the net
+    classifier.add(Dense(  activation='relu',input_dim=11, units= 6, kernel_initializer='uniform' )) # adding layers in hidden layer the average in the input and the output otherwise 11+1/2 we have to do cross validation
+    classifier.add(Dense(  activation='relu', units= 6, kernel_initializer='uniform' )) # adding layers in hidden layer the average in the input and the output otherwise 11+1/2 we have to do cross validation
+    classifier.add(Dense(  activation='sigmoid', units= 1, kernel_initializer='uniform' ))
+    classifier.compile(optimizer=optimizer,loss='binary_crossentropy', metrics=['accuracy']) # adam is stochastic gradient decent, then loss function
+    return classifier
+#return 10 accuaries
+classifier = KerasClassifier(build_fn = build_classifier)
+parameters =  {'batch_size': [25,32], 'epochs':[100,500], 'optimizer': ['adam','rmsprop']}   #the parameters we are trying to tune name of the parameters and then with teh values that we want to test for optimizer a variable is passed in for thed  def
+grid_search =  GridSearchCV(estimator = classifier, 
+                            param_grid= parameters,
+                            scoring = 'accuracy',
+                            cv =10) # from the GridSearchCV CLASS
+grid_search = grid_search.fit(X_train, y_train)
+best_parameters = grid_search.best_params_
+best_accuracy = grid_search.best_score_
 
 
 
