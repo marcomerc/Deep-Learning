7f7a63ec9e5e8b2718d49bd5a866753b3c8b2eee
Marco Mercado <mmerc007@ucr.edu>
2018-03-25 23:38:57 -0700

    started to work on the net and looked at documentation

---
+++ Convolutional_Neural_Networks/con.py
@@ -0,0 +1,28 @@
+# Marco Mercado 
+
+
+# Part 1 - Building the CNN
+
+# Importing the Keras libraries and packages
+from keras.models import Sequential
+from keras.layers import Conv2D # package for the convolutional layers. there is a 3D dimention look at documentation
+from keras.layers import MaxPooling2D #this is for pooling the features for the layers
+from keras.layers import Flatten # converting pool feature maps into the feature vector
+from keras.layers import Dense # it's for the neural network.
+
+#initializing the network
+classifier  = Sequential()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
