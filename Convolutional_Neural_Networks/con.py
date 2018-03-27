0457610d557d0b53667e1c6dcd9ad330657b2195
Marco Mercado <mmerc007@ucr.edu>
2018-03-26 23:01:02 -0700

    convolutional part, max pooling and flattening is done.

--- Convolutional_Neural_Networks/con.py
+++ Convolutional_Neural_Networks/con.py
@@ -12,14 +12,22 @@ from keras.layers import Dense # it's for the neural network.
 
 #initializing the network
 classifier  = Sequential()
-
 #feature detecting it gets a metrix and gives you a feature map and detects a feature.
-classifier.add(Convolution2D())
+classifier.add(Conv2D(32,3,3, input_shape= (64,64,3), activation= "relu"))  # first number is the number of feature maps the other two is the dimentions 3 by 3
+# then input_shape(first how mnay channels color has 3 and back and white has 2 chanels,256 by 256 pixels ) backwords because of tensor 
 
+#pooling reducing the size of the feature map on each of the feauter maps 
+classifier.add(MaxPooling2D(pool_size= (2,2) )) #the size of the feature map by two
 
+#this puts all the feature maps into an array and then get it ready to put it in the neural network.
+classifier.add(Flatten())
 
+#making a regular neural net
+classifier.add(Dense(128, activation = 'relu' ))#first is the output layer size
+classifier.add(Dense(1, activation = 'sigmoid'))#first is the output layer size
 
-
+#compiling 
+classifier.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
 
 
 
