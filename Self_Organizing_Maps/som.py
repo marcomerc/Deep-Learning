87c036a161fc5d64d9a4ad5d466f1d58451c616d
Marco Mercado <mmerc007@ucr.edu>
2018-04-21 22:14:36 -0700

    self organizing maps

--- Self_Organizing_Maps/som.py
+++ Self_Organizing_Maps/som.py
@@ -1,45 +1,32 @@
-# Self Organizing Map
+# -*- coding: utf-8 -*-
 
-# Importing the libraries
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
-
-# Importing the dataset
+#importing the data set
 dataset = pd.read_csv('Credit_Card_Applications.csv')
-X = dataset.iloc[:, :-1].values
-y = dataset.iloc[:, -1].values
+x= dataset.iloc[:,:-1] # the -1 is for the last value 
+y= dataset.iloc[:,-1] # the -1 is for the last value 
 
-# Feature Scaling
+
+
+
+#fesature scaling  Data preprocessing/
 from sklearn.preprocessing import MinMaxScaler
-sc = MinMaxScaler(feature_range = (0, 1))
-X = sc.fit_transform(X)
+sc = MinMaxScaler(feature_range = (0,1))
+X = sc.fit_transform(x) # NORMALIZATION
 
-# Training the SOM
+# we are going to use a libary.
 from minisom import MiniSom
-som = MiniSom(x = 10, y = 10, input_len = 15, sigma = 1.0, learning_rate = 0.5)
+som  = MiniSom(x = 10, y =10, input_len = 15, sigma =1.0, learning_rate = 0.5) 
 som.random_weights_init(X)
-som.train_random(data = X, num_iteration = 100)
+som.train_random(X,100)
 
-# Visualizing the results
-from pylab import bone, pcolor, colorbar, plot, show
-bone()
-pcolor(som.distance_map().T)
-colorbar()
-markers = ['o', 's']
-colors = ['r', 'g']
-for i, x in enumerate(X):
-    w = som.winner(x)
-    plot(w[0] + 0.5,
-         w[1] + 0.5,
-         markers[y[i]],
-         markeredgecolor = colors[y[i]],
-         markerfacecolor = 'None',
-         markersize = 10,
-         markeredgewidth = 2)
-show()
 
-# Finding the frauds
-mappings = som.win_map(X)
-frauds = np.concatenate((mappings[(8,1)], mappings[(6,8)]), axis = 0)
-frauds = sc.inverse_transform(frauds)
+
+
+
+
+
+
+
